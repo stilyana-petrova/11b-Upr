6b2f55d7f2b63cb4a6f81439eb514c9e5fa2282c
stilyana-petrova <s.petrova2018@pmg-pernik.com>
2022-02-08 15:33:47 +0200

    zadachi

---
+++ Masivi/1Zad/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _1Zad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = Console.ReadLine().Split().ToArray();
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 0; i < num; i++)
+            {
+                string[] cmd = Console.ReadLine().Split().ToArray();
+                string command = cmd[0];
+                if (command=="distinct")
+                {
+                    array = array.Distinct().ToArray();
+                }
+                if (command=="reverse")
+                {
+                    array = array.Reverse().ToArray();
+                }
+                if (command=="replace")
+                {
+                    int index = int.Parse(cmd[1]);
+                    string newString=cmd[2];
+                    array[index] = newString;
+                }
+            }
+            Console.WriteLine(string.Join(", ", array));
+        }
+    }
+}
---
+++ Masivi/2Zad/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace _2Zad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = Console.ReadLine().Split().ToArray();
+            while (true)
+            {
+                var cmd = Console.ReadLine().Split().ToArray();
+                string command = cmd[0];
+                if (command=="END")
+                {
+                    break;
+                }
+                switch(command)
+                {
+                    case "Reverse":
+                        arr = arr.Reverse().ToArray();
+                        break;
+                    case "Distinct":
+                        arr = arr.Distinct().ToArray();
+                        break;
+                    case "Replace":
+                        int index = int.Parse(cmd[1]);
+                        string newString = cmd[2];
+                        if (index>=0 && index<arr.Length)
+                        {
+                            arr[index] = newString;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Invalid command");
+                        break;
+                }
+            }
+            Console.WriteLine(string.Join(", ", arr));
+        }
+    }
+}
