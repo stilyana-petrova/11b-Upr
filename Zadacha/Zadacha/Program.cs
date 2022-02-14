adc287580b724b00c115388e2d6d3d70e7763066
stilyana-petrova <s.petrova2018@pmg-pernik.com>
2022-02-14 09:01:49 +0200

    Zadacha14-02

---
+++ Zadacha/Zadacha/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zadacha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
