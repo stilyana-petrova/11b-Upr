cb61892cb3a8c00ac25064161cb3d086ccb2fe3b
stilyana-petrova <s.petrova2018@pmg-pernik.com>
2022-02-14 09:16:24 +0200

    14-02Zadacha

--- Zadacha/Zadacha/Program.cs
+++ Zadacha/Zadacha/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Zadacha
 {
@@ -6,7 +8,40 @@ namespace Zadacha
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            while (true)
+            {
+                string line = Console.ReadLine();
+                if (line=="end")
+                {
+                    break;
+                }
+                string[] tokens = line.Split();
+                switch (tokens[0])
+                {
+                    case "Add":
+                        int numberToAdd = int.Parse(tokens[1]);
+                        numbers.Add(numberToAdd);
+                        break;
+                    case "Remove":
+                        int numberToRemove = int.Parse(tokens[1]);
+                        numbers.Remove(numberToRemove);
+                        break;
+                    case "RemoveAt":
+                        int indexToRemove = int.Parse(tokens[1]);
+                        numbers.RemoveAt(indexToRemove);
+                        break;
+                    case "Insert":
+                        int numberToInsert = int.Parse(tokens[1]);
+                        int indexToInsert = int.Parse(tokens[2]);
+                        numbers.Insert(indexToInsert, numberToInsert);
+                        break;
+
+                    default:
+                        break;
+                }
+                Console.WriteLine(string.Join(" ",numbers));
+            }
         }
     }
 }
