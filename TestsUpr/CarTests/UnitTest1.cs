66b2f10157cdbda72e9671548f36024abb0c05e3
stilyana-petrova <s.petrova2018@pmg-pernik.com>
2022-03-28 10:29:12 +0300

    Zadachi

--- TestsUpr/CarTests/UnitTest1.cs
+++ TestsUpr/CarTests/UnitTest1.cs
@@ -61,5 +61,62 @@ namespace CarTests
             Assert.That(ex.Message, Is.EqualTo("Fuel capacity cannot be zero or negative!"));
         }
         
+        [Test]
+        [TestCase(null, "BMW", 12.5, 20.5)]
+        [TestCase("car", null, 12.5, 20.5)]
+        [TestCase("car", "BMW", 0, 20.5)]
+        [TestCase("car", "BMW", -12.5, 20.5)]
+        [TestCase("car", "BMW", 12.5, 0)]
+        [TestCase("car", "BMW", 12.5, -20.5)]
+        public void ValidateAllProperties(string make, string model, double fuelConsumption, double fuelCapacity)
+        {
+            Assert.Throws<ArgumentException>(() => new Car(make, model, fuelConsumption, fuelCapacity));
+        }
+
+        [Test]
+        public void ShouldRefuelNormally()
+        {
+            Car car = new Car("car", "BMW", 12.5, 20.0);
+           car.Refuel(20);
+            double expectedFuelAmount = 20;
+            double actualFuelFuelAmount = car.FuelAmount;
+            Assert.AreEqual(expectedFuelAmount, actualFuelFuelAmount);
+        }
+
+        [Test]
+        public void ShouldRefuelUntilTotalFuelCapacity()
+        {
+            Car car = new Car("car", "BMW", 12.5, 100);
+            car.Refuel(150);
+            double expectedFuelAmount = 100;
+            double actualFuelFuelAmount = car.FuelAmount;
+            Assert.AreEqual(expectedFuelAmount, actualFuelFuelAmount);
+        }
+
+        [Test]
+        [TestCase(-10)]
+        [TestCase(0)]
+        public void ShouldRefuelThrowArgExWhenInputAmountIsBellowZero(double inputAmount)
+        {
+            Car car = new Car("car", "BMW", 12.5, 100);
+            Assert.Throws<ArgumentException>(() => car.Refuel(inputAmount));
+        }
+
+        [Test]
+        public void ShouldDriveNormally()
+        {
+            Car car = new Car("car", "BMW", 2, 100);
+            car.Refuel(20);
+            car.Drive(20);
+            double expectedFuelAmount = 19.6;
+            double actualFuelFuelAmount = car.FuelAmount;
+            Assert.AreEqual(expectedFuelAmount, actualFuelFuelAmount);
+        }
+        [Test]
+        public void DriveShouldThrowInvalidOperationExceptionWhenFuelAmountIsNotEnough()
+        {
+            Car car = new Car("car", "BMW", 2, 100);
+            Assert.Throws < InvalidOperationException>(() => car.Drive(20));
+        }
     }
 }
