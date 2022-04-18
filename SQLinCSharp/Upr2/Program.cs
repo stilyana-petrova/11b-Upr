c3dd5503b845ea74ce448c0c21ce4ec1a0c72eb7
stilyana-petrova <s.petrova2018@pmg-pernik.com>
2022-04-18 10:28:41 +0300

    Zadachi

---
+++ SQLinCSharp/Upr2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Upr2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var conn = new SqlConnection(@"Server=DESKTOP-G29VUF0\SQLEXPRESS;Database=Minions;Integrated Security=true"))
+            {
+                conn.Open();
+
+                SqlCommand command1 = new SqlCommand
+                    (
+                    "SELECT V.Name, COUNT(MV.VillainId) "+
+                    "FROM Villains as V "+
+                    "JOIN MinionsVillains AS MV on MV.VillainId=V.Id "+
+                    "GROUP BY MV.VillainId, V.Name "+
+                    "HAVING COUNT(MV.VillainId) > 3", conn
+                    );
+                var reader = command1.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{reader[0]} -> {reader[1]}");
+                }
+            }
+        }
+    }
+}
---
+++ SQLinCSharp/Upr3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Upr3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int villainID = int.Parse(Console.ReadLine());
+            
+            using(var conn= new SqlConnection(@"Server=DESKTOP-G29VUF0\SQLEXPRESS;Database=Minions;Integrated Security=true"))
+            {
+                conn.Open();
+                SqlCommand sql = new SqlCommand
+                    (
+                    "SELECT minions.name "+
+                    "FROM minionsvillains "+
+                    "JOIN minions on minionsvillains.MinionId = minions.id "+
+                    $"WHERE minionsvillains.VillainId = @villainID",conn
+                    );
+
+                sql.Parameters.AddWithValue("@villainID", villainID);
+                var reader = sql.ExecuteReader();
+
+                bool hasNames = false;
+                int i = 1;
+                while (reader.Read())
+                {
+                    hasNames = true;
+                    Console.WriteLine($"{0}. {1}", i++, reader[0]);
+                }
+                if (hasNames==false)
+                {
+                    Console.WriteLine($"No villain with ID {villainID} exists in the database.");
+                }
+            }
+        }
+    }
+}
