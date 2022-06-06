3a198660bcceb64a770ea5acdf5a360c402680dc
stilyana-petrova <s.petrova2018@pmg-pernik.com>
2022-06-06 10:10:49 +0300

    Zadachi

---
+++ DbProductsORM/DbProductsORM/Data/ProductContext.cs
@@ -0,0 +1,23 @@
+﻿using DbProductsORM.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DbProductsORM.Data
+{
+    public class ProductContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=DESKTOP-G29VUF0\SQLEXPRESS;Database=ProductDb;Integrated Security=true");
+            }
+        }
+
+        
+    }
+}
---
+++ DbProductsORM/DbProductsORM/Program.cs
@@ -0,0 +1,16 @@
+﻿using DbProductsORM.Data;
+using DbProductsORM.Presentation;
+using System;
+
+namespace DbProductsORM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductContext db = new ProductContext();
+            db.Database.EnsureCreated();
+            Display display = new Display();
+        }
+    }
+}
